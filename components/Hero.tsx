a3d8b87805a28b85e6070894b6a9bcd6e30e940d
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-02-22 23:08:50 +0530

    Add Mobile responsive Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="bg-orange-100 py-9 pt-16 px-4 w-full h-full text-left grid gap-4">
+      <span className="font-RubikRegular text-[20px] text-black ">
+        Interior Needs
+      </span>
+      <h1 className="text-[2rem] text-black font-PollerOne font-bold leading-[2.5rem]">
+        Various new collections of furniture to decorate the corner of your
+        house.
+      </h1>
+      <button className="text-[20px] font-RubikBold font-bold bg-zinc-800 px-5 py-4 text-white">
+        Shop Now
+      </button>
+      <div>
+        <Image
+          src="/../assets/images/HeroFurniture.png"
+          alt="HeroFurniture"
+          width={500}
+          height={500}
+        ></Image>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
