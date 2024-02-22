60b3983a26e3fb0b00593e70dffeaa0d72cceadf
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-02-22 23:02:03 +0530

    Add initial components to the main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import Brands from '@/components/Brands';
+import Hero from '@/components/Hero';
+import NavBar from '@/components/NavBar';
+import LivingRoomSet from '@/components/LivingRoomSet';
+import NewestProducts from '@/components/NewestProducts';
+import InteriorModern from '@/components/InteriorModern';
+import NewArrival from '@/components/NewArrival';
+import Guarantee from '@/components/Guarantee';
+import RecentBlog from '@/components/RecentBlog';
+import Subscribe from '@/components/Subscribe';
+import Footer from '@/components/Footer';
 
 const page = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-white">
+      <NavBar />
+      <Hero />
+      <Brands />
+      <LivingRoomSet />
+      <NewestProducts />
+      <InteriorModern />
+      <NewArrival />
+      <Guarantee />
+      <RecentBlog />
+      <Subscribe />
+      <Footer />
+    </div>
+  );
 };
 
 export default page;
