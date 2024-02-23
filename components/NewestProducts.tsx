c5aca0c36a5af3ba9f1b71c163f06ada387a43d7
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-02-23 17:08:05 +0530

    Add mobile rexponsive NewestProducts section

---
+++ components/NewestProducts.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Image from 'next/image';
+import data from '@/constants/data';
+
+const NewestProducts = () => {
+  return (
+    <section className="px-4 space-y-6 mt-16">
+      <h1 className="text-center font-RalewayRomanBold text-[2.1rem] font-semibold">
+        Our Newest Product
+      </h1>
+      <p className="text-center font-RubikMedium text-[18px] text-neutral-400">
+        Made of the best materials and with a design that follows the times
+      </p>
+      <div className="grid gap-6">
+        {data.NewestProducts.map((product, id) => (
+          <div className="teak_wood w-full h-full" key={id}>
+            <div className="chair relative">
+              <span className="absolute right-3 px-3 py-0.5 top-3 bg-red-500 text-white">
+                New
+              </span>
+              <span className="absolute bg-gray-800 px-2 py-1 text-white text-[0.9rem] bottom-3 left-3 font-RubikRegular">
+                Living Room
+              </span>
+              <Image
+                src={product.img}
+                className="w-full h-96 object-cover"
+                width={100}
+                height={100}
+                alt="teak_wood"
+              ></Image>
+            </div>
+            <div className="flex justify-between pt-7 ">
+              <span className=" font-RalewayRomanBold text-[1.1rem] text-black font-medium ">
+                Teak Wood chair
+              </span>
+              <span className=" font-RalewayRomanBold text-[1.1rem] text-black font-medium ">
+                $24
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default NewestProducts;
