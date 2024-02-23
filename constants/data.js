78da808939ebefba83926b99fadfd5d77df7a7f7
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-02-23 17:07:22 +0530

    Add constants to populate the datas for respective components

---
+++ constants/data.js
@@ -0,0 +1,137 @@
+import Chair01 from '@/public/assets/images/teak_wood01.png';
+import Chair02 from '@/public/assets/images/teak_wood02.png';
+import Chair03 from '@/public/assets/images/teak_wood03.png';
+import Chair04 from '@/public/assets/images/teak_wood04.png';
+import Chair05 from '@/public/assets/images/teak_wood05.png';
+import Chair06 from '@/public/assets/images/teak_wood06.png';
+import Chair07 from '@/public/assets/images/teak_wood07.png';
+import NewArrival01 from '@/public/assets/images/NewArrival01.png';
+import NewArrival02 from '@/public/assets/images/NewArrival02.png';
+import NewArrival03 from '@/public/assets/images/NewArrival03.png';
+import NewArrival04 from '@/public/assets/images/NewArrival04.png';
+import NewArrival05 from '@/public/assets/images/NewArrival05.png';
+import NewArrival06 from '@/public/assets/images/NewArrival06.png';
+import NewArrival07 from '@/public/assets/images/NewArrival07.png';
+import NewArrival08 from '@/public/assets/images/NewArrival08.png';
+import icon01 from '@/public/assets/images/rocket-icon.svg';
+import icon02 from '@/public/assets/images/lock-icon.svg';
+import icon03 from '@/public/assets/images/return-icon.svg';
+import icon04 from '@/public/assets/images/phone-icon.svg';
+import BlogImg01 from '@/public/assets/images/blogimg01.png';
+import BlogImg02 from '@/public/assets/images/blogimg02.png';
+import BlogImg03 from '@/public/assets/images/blogimg03.png';
+
+const NewestProducts = [
+  {
+    id: 'Product01',
+    img: Chair01,
+  },
+  {
+    id: 'Product02',
+    img: Chair02,
+  },
+  {
+    id: 'Product03',
+    img: Chair03,
+  },
+  {
+    id: 'Product04',
+    img: Chair04,
+  },
+  {
+    id: 'Product05',
+    img: Chair05,
+  },
+  {
+    id: 'Product06',
+    img: Chair06,
+  },
+  {
+    id: 'Product07',
+    img: Chair07,
+  },
+];
+
+const NewArrivals = [
+  {
+    id: 'NewArrival01',
+    img: NewArrival01,
+  },
+  {
+    id: 'NewArrival02',
+    img: NewArrival02,
+  },
+  {
+    id: 'NewArrival03',
+    img: NewArrival03,
+  },
+  {
+    id: 'NewArrival04',
+    img: NewArrival04,
+  },
+  {
+    id: 'NewArrival05',
+    img: NewArrival05,
+  },
+  {
+    id: 'NewArrival06',
+    img: NewArrival06,
+  },
+  {
+    id: 'NewArrival07',
+    img: NewArrival07,
+  },
+  {
+    id: 'NewArrival08',
+    img: NewArrival08,
+  },
+];
+
+const Guarantees = [
+  {
+    icon: icon01,
+    head: 'Fast Shipping',
+    body: "With our lightning-fast shipping, your orders are in motion as soon as you hit 'confirm.'",
+  },
+  {
+    icon: icon02,
+    head: 'Safe Delivery',
+    body: 'Our commitment to safe delivery means your items arrive in pristine condition, ready to enhance your life.',
+  },
+  {
+    icon: icon03,
+    head: '365 Days Return',
+    body: 'For any reason, return within a year for hassle-free exchanges.',
+  },
+  {
+    icon: icon04,
+    head: '24 hours CS',
+    body: 'Our customer service is here 24/7 to ensure your experience is seamless.',
+  },
+];
+
+const BlogsSection = [
+  {
+    img: BlogImg01,
+    title: 'Why should you choose good wood',
+    author: 'By Admin',
+    date: 'December 10,2022',
+    body: 'Crafted with care, our wood products stand the test of time, offering enduring beauty and reliability.',
+  },
+  {
+    img: BlogImg02,
+    title: 'Why should you choose good wood',
+    author: 'By Admin',
+    date: 'December 10,2022',
+    body: 'We prioritize eco-friendly practices, ensuring our wood comes from responsibly managed forests, promoting environmental conservation.',
+  },
+  {
+    img: BlogImg03,
+    title: 'Why should you choose good wood',
+    author: 'By Admin',
+    date: 'December 10,2022',
+    body: 'Each piece is meticulously crafted by skilled artisans, resulting in furniture of exceptional quality and craftsmanship.',
+  },
+];
+
+export default { NewestProducts, NewArrivals, Guarantees, BlogsSection };
